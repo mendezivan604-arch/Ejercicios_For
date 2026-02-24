28c5dfd681f287da9828002a40c25b339684495f
Ivan mendez <mendezivan604@gmail.com>
2026-02-24 17:40:06 -0600

    Ejercicio 4

--- Ejercicios_For/Program.cs
+++ Ejercicios_For/Program.cs
@@ -1,17 +1,26 @@
-﻿//Ejercicio 1
+﻿Console.WriteLine("Ejercicio 1");
+//Ejercicio 1
 for (int i = 1; i <= 10; i++)
 {
     Console.WriteLine($"Número: {i}");
 }
-
+Console.WriteLine("Ejercicio 2");
 //Ejercicio 2
 for (int i = 1; i <= 20; i++)
 {
     Console.Write($"{i} ");
 }
-
+Console.WriteLine(" ");
+Console.WriteLine("Ejercicio 3");
 //Ejercicio 3
 for (int i2 = 10; i2 >= 1; i2--)
+{
+    Console.WriteLine($"Número: {i2}");
+}
+
+Console.WriteLine("Ejercicio 4");
+//Ejercicio 4
+for (int i2 = 0; i2 <= 20; i2+=2)
 {
     Console.WriteLine($"Número: {i2}");
 }
